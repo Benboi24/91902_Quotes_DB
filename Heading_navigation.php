57160a563ef9fb52c6c6de617ef2158cd1271f65
Benboi24 <60630518+Benboi24@users.noreply.github.com>
2021-08-17 10:42:40 +1200

    Heading_navigation + showall
    updating and adding

--- Heading_navigation.php
+++ Heading_navigation.php
@@ -1,6 +1,51 @@
 <!DOCTYPE HTML>
 
+<?php
+
+session_start();
+include("config.php");
+include("functions.php");
+
+// Connect to database...
+$dbconnect=mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD_ DB_NAME);
+
+if(mysqli_connect_errno()) {
+    echo "Connection failed:".mysqli_connect_error();
+    exit;
+}
+
+?>
+
+
 <html lang="en">
+
+<?php include("contnent/head.php"); ?>
+
+<body>
+
+    <div class="wrapper">
+
+            <?php include("content/heading_navigation.php");?>
+
+        <div class="box main">
+
+        <?php>
+
+        if(!isset($_REQUEST['page'])) {
+            include("content/home.php");
+        } // end of if that includes home page
+
+        else {
+            // prevents users from navigation through file system
+            $page=preg_replace('/[^0-9a-zA-Z]-/', '', $_REQUEST['page']);
+            include("content/$page.php");
+
+        } // End of else but that includes requested content
+
+        ?>
+
+        </div> <!-- / main -->
+
 
 <head>
     <meta charset="UTF-8">
@@ -39,7 +84,7 @@
             
             <div class="linkwrapper">
                 <div class="commonsearches">
-                    <a href="#l">All</a> | 
+                    <a href="index.php?page=showall">All</a> | 
                     <a href="#">Recent</a> | 
                     <a href="#">Random</a> 
                 </div>  <!-- / common searches -->
---
+++ showall.php
@@ -0,0 +1,27 @@
+
+<h2>All Results</h2>
+
+<?php
+$find_sql = "SELECT * FROM `quotes`
+JOIN author ON (`author`.`Author_ID` = `quotes`.`Author_ID`)
+";
+$find_query = mysqli_query($dbconnect, $find_sql);
+$find_rs = mysqli_fetch_assoc($find_query);
+
+// Loop through results and display them
+do {
+
+    ?>
+<div class="results">
+    <?php echo $find_rs['Quote']; ?>
+</div>
+
+<br />
+
+<?php
+
+} // End of display results 'do'
+
+while($find_rs = mysqli_fetch_assoc($find_query))
+
+?>
